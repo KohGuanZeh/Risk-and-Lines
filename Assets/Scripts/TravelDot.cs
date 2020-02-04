849962fc83c0c17e0cd0888b50326664cc4cdb19
skyrabbit13 <reynardcheng@gmail.com>
2020-02-04 19:38:08 +0800

    edited player and dot script. implemented player move and blink

--- Assets/Scripts/TravelDot.cs
+++ Assets/Scripts/TravelDot.cs
@@ -6,4 +6,7 @@ public class TravelDot : MonoBehaviour
 {
 	[Header("Travel Dot Properties")]
 	public bool locked; //Check if Dot is already being locked by a Player
+
+	public Player currentPlayer;
+	public int blinkCount;
 }
