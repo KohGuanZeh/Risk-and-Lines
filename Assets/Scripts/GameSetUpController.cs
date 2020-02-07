23504d58a547eb4123f2000d5a680882abd1ced2
skyrabbit13 <reynardcheng@gmail.com>
2020-02-07 17:46:02 +0800

    added game lobby

---
+++ Assets/Scripts/GameSetUpController.cs
@@ -0,0 +1,19 @@
+﻿
+using UnityEngine;
+using Photon.Pun;
+using System.IO;
+
+public class GameSetUpController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+		CreatePlayer();
+    }
+
+   private void CreatePlayer()
+	{
+		Debug.Log("Creating Player");
+		PhotonNetwork.Instantiate(Path.Combine("PhotonPrefabs", "Player"), Vector3.zero, Quaternion.identity);
+	}
+}
---
+++ Assets/Scripts/LobbyManager.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+using Photon.Realtime;
+using UnityEngine.UI;
+
+public class LobbyManager : MonoBehaviourPunCallbacks
+{
+	[SerializeField]
+	private GameObject UserNameScreen, ConnectScreen;
+
+	[SerializeField]
+	private InputField UserNameInput, CreateRoomInput, JoinRoomInput;
+
+	[SerializeField]
+	private GameObject CreateUserNameButton;
+
+	[SerializeField] GameObject characterSelect;
+
+
+	// for quick play
+	public GameObject quickPlay,quickPlayCancel;
+	[SerializeField] private int roomSize;
+	private void Start()
+	{
+		//Connect to server using the PhotonServerSettings
+		PhotonNetwork.ConnectUsingSettings();
+	}
+
+	// Called when the client is connected to the Master Server and ready for matchmaking and other tasks.
+	public override void OnConnectedToMaster()
+	{
+		//Debug.Log("Connected to Master Server!!!");
+		//PhotonNetwork.JoinLobby(TypedLobby.Default);
+		PhotonNetwork.AutomaticallySyncScene = true;
+		quickPlay.SetActive(true);
+	}
+
+	// this is to quick start the game 
+	public void QuickStart()
+	{
+		quickPlay.SetActive(false);
+		quickPlayCancel.SetActive(true);
+		PhotonNetwork.JoinRandomRoom();
+		print("quickStarted");
+	}
+	public void QuickCancel()
+	{
+		quickPlayCancel.SetActive(false);
+		quickPlay.SetActive(true);
+		PhotonNetwork.LeaveRoom();
+	}
+	public override void OnJoinRandomFailed(short returnCode, string message)
+	{
+		//base.OnJoinRandomFailed(returnCode, message);
+		CreateRoom();
+	}
+	void CreateRoom()
+	{
+		int randomRoomNummber = Random.Range(0, 1000);//creating a random room name for the room 
+		RoomOptions roomOps = new RoomOptions() { IsVisible = true, IsOpen = true, MaxPlayers = (byte)roomSize };
+		PhotonNetwork.CreateRoom("Room" + randomRoomNummber, roomOps);
+		print(randomRoomNummber);
+	}
+
+	public override void OnCreateRoomFailed(short returnCode, string message)
+	{
+		Debug.Log("Failed to create room... trying again");
+		CreateRoom();
+	}
+}
