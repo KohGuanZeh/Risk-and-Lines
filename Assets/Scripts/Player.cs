06ffd5349eaaf63e5d8d78c5d0c5cefeca76f0c3
Koh Guan Zeh <kohguanzeh960@gmail.com>
2020-02-07 15:42:43 +0800

    Added Travel Line Class and Set Up Colliders for Lines and Players

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -6,11 +6,11 @@ public class Player : MonoBehaviour
 {
 	[Header("General Player Properties")]
 	[SerializeField] GameManager gm;
-    [SerializeField] LineRenderer linePreset; //Stores the Line Prefab that it will Instantiate each time Player travels
+    [SerializeField] TravelLine linePreset; //Stores the Line Prefab that it will Instantiate each time Player travels
 
     [Header("Player Movement Properties")]
     [SerializeField] float travelSpeed = 5; //Speed at which the Player Dot travels
-	[SerializeField] LineRenderer currentTravelLine; //The Line 
+	[SerializeField] TravelLine currentTravelLine; //The Line 
 	[SerializeField] TravelDot targetDot, storedDot; //Target Dot is which Dot Player will travel to. Stored Dot is The Dot that is currently tapped
 	[SerializeField] float doubleTapThreshold; //this is the time before travel and also the time to register a double tap
 	[SerializeField] float distanceToStop;
@@ -51,16 +51,11 @@ public class Player : MonoBehaviour
 						travelDot.locked = true;
 						targetDot = travelDot;
 
-						if (currentTravelLine)
-						{
-							currentTravelLine.positionCount++;
-							currentTravelLine.SetPosition(currentTravelLine.positionCount - 1, transform.position);
-						}
+						if (currentTravelLine) currentTravelLine.AddNewPoint(transform.position);
 						else
 						{
-							currentTravelLine = Instantiate(linePreset, transform); //Will be changed to Object Pooling
-							currentTravelLine.positionCount = 2;
-							for (int i = 0; i < currentTravelLine.positionCount; i++) currentTravelLine.SetPosition(i, transform.position);
+							currentTravelLine = ObjectPooling.inst.SpawnFromPool("Line", transform.position, Quaternion.identity).GetComponent<TravelLine>(); //Instantiate(linePreset, transform); //Will be changed to Object Pooling
+							currentTravelLine.CreateNewLine(transform.position);
 						}
 					} 
 					else if (travelDot == storedDot && doubleTapThreshold > 0) //If there is already a Stored Dot, This is Considered a Double Tap
@@ -90,35 +85,37 @@ public class Player : MonoBehaviour
 		if (targetDot == null) return;
 
 		transform.position = Vector2.MoveTowards(transform.position, targetDot.transform.position, travelSpeed * Time.deltaTime);
-		currentTravelLine.SetPosition(currentTravelLine.positionCount - 1, transform.position);
+		currentTravelLine.UpdateLine(transform.position);
 
 		//To remove the targetDot
 		float distanceToDot = Vector2.Distance(transform.position, targetDot.transform.position);
-		if (distanceToDot <= distanceToStop)
-		{
-			targetDot = null;
-			distanceToStop = 0f;
-		}
+		if (distanceToDot <= distanceToStop) targetDot = null;
 	}
 
 	void BlinkControl()
 	{
-		//if (targetDot != null) //If Player is Travelling. Rmb Delete the Line Renderer
 		//Insert Camera Shake Effect
+
+		CutLine(); //Need to be on top before Target Dot is Set to Null
 
 		transform.position = storedDot.transform.position;
 		targetDot.locked = false;
 		targetDot = null;
 		storedDot = null;
-		CutLine();
 		doubleTapThreshold = 0;
 	}
 
 	//Delete the last position and cut out the Travel Line
 	void CutLine()
 	{
-		if (distanceToStop <= 0) currentTravelLine.positionCount--;
-		if (currentTravelLine.positionCount == 1) Destroy(currentTravelLine);
+		if (targetDot != null) currentTravelLine.CutLine();
 		currentTravelLine = null;
+	}
+
+	public void Death()
+	{
+		print("Player is Dead");
+		//gameObject.SetActive(false);
+		//Check Player Count. If Player Count is 1. Trigger Win Screen
 	}
 }
