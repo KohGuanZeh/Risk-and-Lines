bd3edff497f12c23fc205215d1423a817811b171
skyrabbit13 <reynardcheng@gmail.com>
2020-02-10 02:44:00 +0800

    merge

--- Assets/Scripts/CamShake.cs
+++ Assets/Scripts/CamShake.cs
@@ -4,15 +4,10 @@ using UnityEngine;
 
 public class CamShake : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+	public Animator camAnim;
+	
+	void Shake()
+	{
+		camAnim.SetTrigger("Shake");
+	}
 }
