7182daf0d6946db1fa3aeca15f8bf807bca37896
skyrabbit13 <reynardcheng@gmail.com>
2020-02-09 18:34:15 +0800

    lobby done

--- Assets/Scripts/PhotonSceneScripts/CustomMatchMakingRoomController.cs
+++ Assets/Scripts/PhotonSceneScripts/CustomMatchMakingRoomController.cs
@@ -6,18 +6,88 @@ using Photon.Pun;
 using Photon.Realtime;
 using UnityEngine.UI;
 
-public class CustomMatchMakingRoomController : MonoBehaviour
+public class CustomMatchMakingRoomController : MonoBehaviourPunCallbacks
 {
-	[SerializeField]
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+	[SerializeField] private int gameLevelSceneIndex; // for loading the game level
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+	[SerializeField] private GameObject lobbyPanel;
+	[SerializeField] private GameObject roomPanel;
+
+	[SerializeField] private GameObject startButton;
+
+	[SerializeField] private Transform playersContainer; // used to display all the players in the current room
+
+	[SerializeField] private GameObject playerListingPrefab; // Instantiate to display each player in the room
+
+	[SerializeField] private Text roomNameDisplay; // display the name of the room
+
+	void ClearPlayerListings()
+	{
+		for (int i = playersContainer.childCount - 1; i >= 0; i--) // loops through all the child object of the 
+		{
+			Destroy(playersContainer.GetChild(i).gameObject);
+		}
+	}
+	void ListPlayers()
+	{
+		foreach (Player player in PhotonNetwork.PlayerList)
+		{
+			GameObject tempListing = Instantiate(playerListingPrefab, playersContainer);
+			Text tempText = tempListing.transform.GetChild(0).GetComponent<Text>();
+			tempText.text = player.NickName;
+		}
+	}
+	public override void OnJoinedRoom()
+	{
+		roomPanel.SetActive(true); // activate the display for being in a room
+		lobbyPanel.SetActive(false); // hide the display for being in a lobby
+		roomNameDisplay.text = PhotonNetwork.CurrentRoom.Name; // update the room name display
+		if (PhotonNetwork.IsMasterClient)
+		{
+			startButton.SetActive(true);
+		}
+		else
+		{
+			startButton.SetActive(false);
+		}
+		ClearPlayerListings(); // remove all old player listings
+		ListPlayers(); // relist all current player listings
+	}
+	public override void OnPlayerEnteredRoom(Player newPlayer)
+	{
+		ClearPlayerListings();
+		ListPlayers();
+	}
+	public override void OnPlayerLeftRoom(Player otherPlayer)
+	{
+		ClearPlayerListings();
+		ListPlayers();
+		if (PhotonNetwork.IsMasterClient) // if the local player is now the new mater client then we activate the start button
+		{
+			startButton.SetActive(true);
+		}
+	}
+	public void StartGame()
+	{
+		if (PhotonNetwork.IsMasterClient)
+		{
+			PhotonNetwork.CurrentRoom.IsOpen = false; // comment out if you wan the player to join after the game has started
+			PhotonNetwork.LoadLevel(gameLevelSceneIndex);
+		}
+	}
+	IEnumerator rejoinLobby()
+	{
+		yield return new WaitForSeconds(1);
+		PhotonNetwork.JoinLobby();
+	}
+	public void BackOnClick() // to avoid the issue of the lobby not updating the rooms
+	{
+		lobbyPanel.SetActive(true);
+		roomPanel.SetActive(false);
+		PhotonNetwork.LeaveRoom();
+		PhotonNetwork.LeaveLobby();
+		StartCoroutine(rejoinLobby());
+	}
 }
+	
+
