d3e136b97c446e9f35bcee508d063654196b2459
skyrabbit13 <reynardcheng@gmail.com>
2020-02-16 16:14:18 +0800

    Stash changes

--- Assets/Scripts/ChatScripts/ChatManager.cs
+++ Assets/Scripts/ChatScripts/ChatManager.cs
@@ -11,10 +11,14 @@ public class ChatManager : MonoBehaviourPun {
 	Player player;
 	public InputField chatInput;
 	public Transform chatContainer; // to hold all the chat messages
-	public Text playerName;
 	public GameObject textPrefab;
+	public RectTransform chatRect;
 
-
+	private void Update() {
+		if (Input.GetKeyDown(KeyCode.KeypadEnter)) {
+			deliverMsg();
+		}
+	}
 	public void deliverMsg() {
 		photonView.RPC("SendMsg", RpcTarget.All, PhotonNetwork.NickName + ": " + chatInput.text); // to send the others this message
 	}
@@ -22,12 +26,14 @@ public class ChatManager : MonoBehaviourPun {
 	[PunRPC]
 	//sends the message to the chat box
 	void SendMsg(string msg) {
-		if (msg != "" && msg.Length >= 1) {
+		if (msg != PhotonNetwork.NickName + ": " + "" && msg.Length > 1) {
 			GameObject textObj = PhotonNetwork.Instantiate(System.IO.Path.Combine("PhotonPrefabs", "MessagePrefab"), Vector2.zero, Quaternion.identity);
 			//GameObject textObj = Instantiate(textPrefab,chatContainer);
 			textObj.GetComponent<Text>().text = msg; // sets the text of the prefab and acts as a message
 			textObj.transform.parent = chatContainer;
 			textObj.transform.localScale = new Vector3(1, 1, 1);
+
+			chatRect.sizeDelta = new Vector2 (chatRect.sizeDelta.x,chatRect.sizeDelta.y + textObj.GetComponent<RectTransform>().sizeDelta.y);
 			chatInput.text = ""; // clears the text inputtext after the message has been sent
 		}
 	}
