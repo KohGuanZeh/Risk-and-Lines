e9024af0ea2ce13facf9f2259189890ba7eccb79
Koh Guan Zeh <kohguanzeh960@gmail.com>
2020-02-25 04:48:09 +0800

    Testing Travel Line

--- Assets/Scripts/TravelLine.cs
+++ Assets/Scripts/TravelLine.cs
@@ -114,6 +114,7 @@ public class TravelLine : MonoBehaviour, IPooledObject
 
 	void OnTriggerEnter2D(Collider2D other)
 	{
+		print("Triggered");
 		if (other.CompareTag("Player"))
 		{
 			PlayerController detectedPlayer = other.GetComponent<PlayerController>();
